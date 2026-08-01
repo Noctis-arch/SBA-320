9ec4246d0de41b4b46ce82b79a75213803d1fc10
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-01 17:08:20 -0400

    Render SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <main>
+        <SearchBar />
+      </main>
     </>
   );
 }
