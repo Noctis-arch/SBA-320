558c01925506141e20fbe09f85be7a3e7eb782c5
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-01 19:17:09 -0400

    Support Enter key for Pokemon search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,23 @@
 function SearchBar({ search, setSearch, handleSearch }) {
-  return (
-    <section className="search-section">
-      <input
-        type="text"
-        placeholder="Search for a Pokémon..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+    return (
+        <section className="search-section">
+            <input
+                type="text"
+                placeholder="Search for a Pokémon..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        handleSearch();
+                    }
+                }}
+            />
 
-      <button onClick={handleSearch}>
-        Search
-      </button>
-    </section>
-  );
+            <button onClick={handleSearch}>
+                Search
+            </button>
+        </section>
+    );
 }
 
 export default SearchBar;
