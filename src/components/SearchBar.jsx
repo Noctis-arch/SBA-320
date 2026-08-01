922dc499c7bca0a60ead9755074f32035f5209da
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-01 18:03:13 -0400

    Convert SearchBar into controlled component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,16 @@
-function SearchBar() {
+function SearchBar({ search, setSearch, handleSearch }) {
   return (
     <section className="search-section">
       <input
         type="text"
         placeholder="Search for a Pokémon..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
 
-      <button>Search</button>
+      <button onClick={handleSearch}>
+        Search
+      </button>
     </section>
   );
 }
