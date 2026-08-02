34980774c4b9e9f0b75c6781a9e4f3d2e59b8275
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-01 23:33:57 -0400

    Persist favorites using localStorage

--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -3,16 +3,18 @@ import { createContext, useEffect, useState } from "react";
 export const PokemonContext = createContext();
 
 function PokemonProvider({ children }) {
-const [favorites, setFavorites] = useState(() => {
-  const savedFavorites = localStorage.getItem("favorites");
+  const [favorites, setFavorites] = useState(() => {
+    const savedFavorites = localStorage.getItem("favorites");
 
-  return savedFavorites ? JSON.parse(savedFavorites) : [];
-});
+    return savedFavorites ? JSON.parse(savedFavorites) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
-    <PokemonContext.Provider
-      value={{ favorites, setFavorites }}
-    >
+    <PokemonContext.Provider value={{ favorites, setFavorites }}>
       {children}
     </PokemonContext.Provider>
   );
