9c5b075144ea9bb70fff571a2d795b48cbda8240
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-01 23:29:33 -0400

    Load favorites from localStorage

--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -1,9 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const PokemonContext = createContext();
 
 function PokemonProvider({ children }) {
-  const [favorites, setFavorites] = useState([]);
+const [favorites, setFavorites] = useState(() => {
+  const savedFavorites = localStorage.getItem("favorites");
+
+  return savedFavorites ? JSON.parse(savedFavorites) : [];
+});
 
   return (
     <PokemonContext.Provider
